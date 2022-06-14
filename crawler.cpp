a5f375e5909ca63d24a80c93cdac07bed83fc651
maikaedberg <69947279+maikaedberg@users.noreply.github.com>
2022-06-14 15:18:35 +0200

    fixed

--- crawler.cpp
+++ crawler.cpp
@@ -3,7 +3,6 @@
 #include <chrono>
 #include <cstring>
 
-template <typename T>
 size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream){
     // link:= <a href="{link}"*
 
@@ -14,10 +13,7 @@ size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream){
 
     while(regex_search(htmlfile, m, regexp_atg)){ 
         for (auto x : m){
-            std::tuple<CrawlerStruct<T>, HtmlLink>* myInfo = (std::tuple<CrawlerStruct<T>, HtmlLink> *) stream ;
-            CrawlerStruct<T> myCrawler = std::get<0>(*myInfo);
-            HtmlLink firstLink = std::get<1>(*myInfo);
-            myCrawler.add( HtmlLink ( firstLink, x) );
+            ((std::vector<std::string>*)stream)->push_back(x);
         }
         htmlfile = m.suffix() ;
     }
@@ -32,17 +28,19 @@ void crawl_website(CrawlerStruct<T> myCrawler, HtmlLink startlink, bool verbose)
     curl_handle = curl_easy_init();
 
     curl_easy_setopt(curl_handle, CURLOPT_URL, startlink.url.c_str());
-    curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, &write_data<T>);
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, &write_data);
 
-    std::tuple<CrawlerStruct<T>, HtmlLink> myInfo = std::make_tuple(myCrawler, startlink);
-    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, &myInfo);
+    std::vector<std::string> links;
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, &links);
     auto start = std::chrono::system_clock::now();
     curl_easy_perform(curl_handle);
 
     auto finish = std::chrono::system_clock::now();
     auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(finish - start).count();
 
-    std::cout << "Crawler " << myCrawler.size() << " " << elapsed << std::endl;
+    for (auto link : links){
+        myCrawler.add( HtmlLink( startlink, link) );
+    }
 
     curl_easy_cleanup(curl_handle);
  
